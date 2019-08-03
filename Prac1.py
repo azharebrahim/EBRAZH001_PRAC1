3041ecb5ebe0b1c0e0948b02416fa8a0b2d03987
azharebrahim <EBRAZH001@myuct.ac.za>
2019-08-03 16:10:12 +0100

    Function for Up counter

--- Prac1.py
+++ Prac1.py
@@ -6,16 +6,35 @@ Date: July
 
 """
 import RPi.GPIO as GPIO
-from time import sleep
+from time import sleep            #import libraries
 
 print "3-Bit Binary Counter"
 
 GPIO.setwarnings(0)
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)           #set to board mode
 
-GPIO.setup(33, GPIO.OUT)
+GPIO.setup(33, GPIO.OUT)           #init LED's
 GPIO.setup(37, GPIO.OUT)
 GPIO.setup(7,  GPIO.OUT)
 
-GPIO.setup(16, GPIO.IN, pull_up_down= GPIO.PUD_UP)
+GPIO.setup(16, GPIO.IN, pull_up_down= GPIO.PUD_UP)           #init push buttons
 GPIO.setup(18, GPIO.IN, pull_up_down= GPIO.PUD_UP)
+
+x=0
+cnt=0
+y=0
+
+def my_callback_one(channel1):                                              #function definition
+			global cnt
+			x=0
+			y=0
+
+			if GPIO.event_detected(16):                          #check if butto pressed
+				print "Pressed 1"
+				x=1
+
+				cnt+=1%8
+				GPIO.output(33, cnt & 0x01)                     #upward counter output to LED's
+				GPIO.output(37, cnt & 0x02)
+				GPIO.output(7,  cnt & 0x04)
+				sleep(0.1)                                      #sleep function of 0.1 seconds
