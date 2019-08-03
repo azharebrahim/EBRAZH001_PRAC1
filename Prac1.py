6cc1d4e4f939ed453cb06c38ff773dad5d6f738a
azharebrahim <EBRAZH001@myuct.ac.za>
2019-08-03 15:58:16 +0100

    Initialised I/O

--- Prac1.py
+++ Prac1.py
@@ -5,3 +5,17 @@ Prac: 1
 Date: July
 
 """
+import RPi.GPIO as GPIO
+from time import sleep
+
+print "3-Bit Binary Counter"
+
+GPIO.setwarnings(0)
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(33, GPIO.OUT)
+GPIO.setup(37, GPIO.OUT)
+GPIO.setup(7,  GPIO.OUT)
+
+GPIO.setup(16, GPIO.IN, pull_up_down= GPIO.PUD_UP)
+GPIO.setup(18, GPIO.IN, pull_up_down= GPIO.PUD_UP)
