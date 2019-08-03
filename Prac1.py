c6ef76e7c0df31d24d52c970835acbbd1146f643
azharebrahim <EBRAZH001@myuct.ac.za>
2019-08-03 16:21:52 +0100

    Function for down counter

--- Prac1.py
+++ Prac1.py
@@ -38,3 +38,22 @@ def my_callback_one(channel1):                                              #fun
 				GPIO.output(37, cnt & 0x02)
 				GPIO.output(7,  cnt & 0x04)
 				sleep(0.1)                                      #sleep function of 0.1 seconds
+
+def my_callback_two(chanel2):
+			global cnt
+			y=0
+			if GPIO.event_detected(18):
+				print "Pressed 2"
+				y=1
+
+				cnt-=1%8
+				GPIO.output(33, cnt & 0x01)
+				GPIO.output(37, cnt & 0x02)
+				GPIO.output(7,  cnt & 0x04)
+				sleep(0.1)
+
+GPIO.add_event_detect(16,GPIO.RISING, callback=my_callback_one, bouncetime=50)
+GPIO.add_event_detect(18,GPIO.RISING, callback=my_callback_two, bouncetime=50)
+
+while(1):
+	c=10
